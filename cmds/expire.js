f9c71b14ba12a034607ee70b72f3c4b37aa5bb7a
nakamuraao <williamhuang0831@gmail.com>
2022-05-07 15:46:31 +0800

    bug

--- cmds/expire.js
+++ cmds/expire.js
@@ -2,16 +2,16 @@ const verifySys = require('../modules/verifySystem');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('expire').setDescription('從會員資料庫移除').addSubcommand(sub=>sub.setName('YouTube').setDescription('從YT會員資料庫移除')).addSubcommand(sub=>sub.setName('TwitCasting').setDescription('從TC資料庫移除')),
+    data: new SlashCommandBuilder().setName('expire').setDescription('從會員資料庫移除').addSubcommand(sub=>sub.setName('youtube').setDescription('從YT會員資料庫移除')).addSubcommand(sub=>sub.setName('twitcasting').setDescription('從TC資料庫移除')),
 
     async execute(interaction){
-        if (interaction.options.getSubcommand() === 'YouTube') {
+        if (interaction.options.getSubcommand() === 'youtube') {
             if(interaction.member.roles.cache.some(role=>role.id === '951074513905418290')){
                 interaction.reply('你的YT會員未過期，不可自資料庫移除')
             }else{
                 await verifySys.deleteYTUser(interaction.user.id)
                 interaction.reply('已將你從 YT會員 資料庫移除')}
-        }else if (interaction.options.getSubcommand() === 'TwitCasting'){
+        }else if (interaction.options.getSubcommand() === 'twitcasting'){
             if(interaction.member.roles.cache.some(role=>role.id === '947827884364558406')){
                 interaction.reply('你的TC會員未過期，不可自資料庫移除')
             }else{
