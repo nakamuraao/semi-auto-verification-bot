46ef9afdaf45d3221155ab5a1e95f0bfda574b77
nakamuraao <williamhuang0831@gmail.com>
2022-05-14 23:39:32 +0800

    multi-server done

--- cmds/help.js
+++ cmds/help.js
@@ -7,16 +7,13 @@ module.exports = {
     async execute(interaction){
         const helpembed = new MessageEmbed()
 				.setColor('#76afee')
-				.setTitle('半自動會員審核機器人 v1.3.0(2022.5.12)')
-				.setDescription('由 蒼アオ#7022 與 Pierre#9505 製作，有任何問題請洽管理員\n\n')
-        //.addField('取得圖片url','將圖片以附件形式加在訊息裡，訊息內容打=>url，有問題請洽管理員或看別人怎麼做',false)
-				//.addField('審核指令','/verify `圖片url`',false)
-        .addField('將資料自資料庫移除','`/expire`\n如果你不打算續費會員，請使用此指令將自己的資料自資料庫移除，否則機器人會每天提醒你續費',false)
+				.setTitle('半自動會員審核機器人 v2.0.1 (2022.5.14)')
+        .setURL('https://github.com/nakamuraao/semi-auto-verification-bot/tree/main')
+				.setDescription('由 蒼アオ#7022 與 Pierre#9505 製作，有任何問題請洽管理員\n以下是可能使用到的指令\n\n')
         .addField('將一個成員的會員資格取消','`/adminexpire 成員UID`', false)
         .addField('確認機器人存活','`/ping`',false)
         .addField('叫出此訊息','`/help`',false)
-				//.setImage('https://cdn.discordapp.com/attachments/971679867198377994/972025537977520148/unknown.png')
-
+        
         await interaction.reply({embeds:[helpembed]})
     }
 }
